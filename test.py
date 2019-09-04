ec6fff525075cbd3729934f14b51cd981e2a01a9
William Bariout <willbariout@gmail.com>
2019-09-04 09:23:43 +0200

    Test Qt python

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("inchallah")
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Top'))
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
+window.show()
+app.exec_()
