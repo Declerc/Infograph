5fdd8040a1679b5c5f9ae4a0265daa585cc5feda
William Bariout <willbariout@gmail.com>
2019-09-23 10:32:38 +0200

    "Changement nom variables"

--- Python/App.py
+++ Python/App.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 
 
 class App:
-    @property   #Getter Setter
+    @property   # Getter Setter
     def Width(self):
         return self._width
     @Width.setter
@@ -16,7 +16,7 @@ class App:
     def Height(self, value):
         self._height = value
 
-    def __init__(self, width, height): #Constructeur
+    def __init__(self, width, height): # Constructeur
         self.Width = width
         self.Height = height
 
@@ -58,32 +58,31 @@ class App:
 
         Fenetre.config(menu=Menubar)
 
-    def CreateTab(self):                #Fonction pour créer fenetre
+    def CreateTab(self):                # Fonction pour créer fenetre
         TabName = ttk.Frame(tabControl, width=200, height=200)
         tabControl.add(TabName)
         ttk.Label(TabName, text="This is Tab {}".format(tabControl.index(TabName))).grid(column=0, row=0, padx=10, pady=10)
-        tabControl.tab(TabName, text= tabControl.index(TabName)) #Affiche numéro tab dans titre
+        tabControl.tab(TabName, text= tabControl.index(TabName))  # Affiche numéro tab dans titre
 
     def RunFenetre(self):
         Fenetre = Tk()
         Fenetre.title("Infograph")
         Fenetre['bg'] = 'grey'
-            # Mettre en global pour etre reconnu dans createTab
+        # Mettre en global pour etre reconnu dans createTab
 
         Fenetre.geometry("{}x{}".format(self._width, self._height))
 
         self.CreationMenu(Fenetre)
 
-        Fenetre.update_idletasks()
-        w = Canvas(Fenetre, width=Fenetre.winfo_width(), height=Fenetre.winfo_height()).place(x= 0, y = 0)
+        Fenetre.update_idletasks()  # Permet que winfo_height et width ne soit pas égaux à 1.
+        canvasFenetre = Canvas(Fenetre, width=Fenetre.winfo_width(), height=Fenetre.winfo_height()).place(x= 0, y = 0)  # Canvas de toute la fenetre
+        canvasConsole = Canvas(Fenetre, bg = "black", width = 100, height = Fenetre.winfo_height()) # Canvas console
 
-        x = Canvas(Fenetre, bg = "black", width = 100, height = 500)
 
-
-        print(Fenetre.winfo_width(), Fenetre.winfo_height())
-        x.pack(side=RIGHT)
-        global tabControl
-        tabControl = ttk.Notebook(w)
+        # print(Fenetre.winfo_width(), Fenetre.winfo_height())
+        canvasConsole.pack(side=RIGHT)
+        global tabControl  # Global pour être reconnu dans autres fonctions
+        tabControl = ttk.Notebook(canvasFenetre)  # Tabs dépendent du canvas
         tabControl.pack(expand=1, fill="both")  # Pack to make visible
 
         Fenetre.mainloop()
