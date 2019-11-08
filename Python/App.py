10140618a9c531b1cc94624f2eeefce645cfa190
William Bariout <willbariout@gmail.com>
2019-11-08 16:33:09 +0100

    Refonte App.py en App2.py, pas terminé.

--- Python/App.py
+++ Python/App.py
@@ -2,6 +2,19 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
+class MyTab(Frame):
+
+    def __init__(self, root):
+        Frame.__init__(self, root)
+
+        self.root = root
+        #self.name = name
+
+        #self.entry = Entry(self)
+        #self.entry.pack(side=TOP)
+
+        #self.entry.bind('<FocusOut>', self.alert)
+        #self.entry.bind('<Key>', self.printing)
 
 class App:
     @property   # Getter Setter
@@ -20,6 +33,7 @@ class App:
     def __init__(self, width, height): # Constructeur
         self.Width = width
         self.Height = height
+
 
     def CreationMenu(self, Fenetre):
         Menubar = Menu(Fenetre)
@@ -60,38 +74,43 @@ class App:
 
         Fenetre.config(menu=Menubar)
 
-    global canvasTab  # Déclaration tableau qui contient les canvas des tabs
+    #global canvasTab  # Déclaration tableau qui contient les canvas des tabs
     canvasTab = []
 
     def CreateTab(self):                # Fonction pour créer fenetre
+        tab = MyTab(self)
+        self.tabControl.add(tab, text=tabControl.index(tab))
+        tabControl.select(tab)
+
 
         TabName = ttk.Frame(tabControl)
         print(TabName)
         tabControl.add(TabName)
-        ttk.Label(TabName, text="This is Tab {}".format(tabControl.index(TabName))).grid(column=0, row=0, padx=10, pady=10)
-        tabControl.tab(TabName, text= tabControl.index(TabName))  # Affiche numéro tab dans titre
+        tabControl.tab(TabName, text=tabControl.index(TabName))  # Affiche numéro tab dans titre
 
         tabControl.select(TabName)
         self.canvas = Canvas(TabName, width=TabName.winfo_width(), height=TabName.winfo_height(), cursor="cross" ,bg="red")  #Creer zone dessin pour le graph
         self.canvas.grid(row=0, column=0, sticky=N + S + E + W)
+        self.Con_func = tabControl.bind('<Configure>', self.d, "+") #Bind tabName à la fonction qui change la taille des canvas dynamiquement
+        self.canvasTab.append(self.canvas)
 
-        tabControl.bind('<Configure>', self.d) #Bind tabName à la fonction qui change la taille des canvas dynamiquement
+    def start_generating(self):
+        self.tabs['ky'] += 1
+        self.addTab('tab')
 
-        #print(TabName.winfo_width())
-        canvasTab.append(self.canvas)
-
-
-    def DeleteTab(self):
+    def DeleteTab(self): #  FIXME: FINIR POUR ENLEVER BUGS
         tabControl.forget(tabControl.select())
         print("Tab select ", tabControl.select())
         print("Tab frame ", ttk.Frame(tabControl))
         print("Tabs ", tabControl.tabs())
 
-    def handle_tab_changed(self, event): #Detecte si la tab selectioné a changé
+    def handle_tab_changed(self, event):  # TODO: FAIRE EN SORTE QUE TOUTE LES TABS SOIENT RESPONSIVE
         selection = event.widget.select()
+        #tabControl.unbind('<Configure>')
+        print(tabControl)
         tab = event.widget.tab(selection, "text")
-        print("text:", tab)
-        self.canvas.config(width=self.Width, height=self.Height)
+
+        #self.canvas.config(width=self.Width, height=self.Height)
 
     def MenuButtonGraph(self, Fenetre):             #Boutton dessin point a la main
         canvasButton = Canvas(Fenetre, width=Fenetre.winfo_width(), height=25)
@@ -103,8 +122,8 @@ class App:
 
     def CreatePoint(self):          #lie le canvas de la tab et l evenement du click souris
         try:
-            canvasTab[tabControl.index(tabControl.select())].bind("<ButtonPress-1>", self.on_button_press)
-            canvasTab[tabControl.index(tabControl.select())].bind("<ButtonRelease-1>", self.on_button_release)
+            self.canvasTab[tabControl.index(tabControl.select())].bind("<ButtonPress-1>", self.on_button_press)
+            self.canvasTab[tabControl.index(tabControl.select())].bind("<ButtonRelease-1>", self.on_button_release)
         except TclError:
             messagebox.showerror("Erreur Tab", "Vous devez créer une table pour dessiner un graphe : \nFichier > Créer")
 
@@ -112,14 +131,14 @@ class App:
 
 
     def on_button_press(self, event):  #récupère les positions de la souris au click et dessine un point
-        print(tabControl.select())
-        canvasTab[tabControl.index(tabControl.select())].create_oval(event.x - 20, event.y - 20, event.x + 20, event.y + 20,
+        self.canvasTab[tabControl.index(tabControl.select())].create_oval(event.x - 20, event.y - 20, event.x + 20, event.y + 20,
                                                                      fill="blue", outline="#DDD", width=4)
 
     def on_button_release(self, event):
         pass
 
     def d(self, event):     #Permet que la taille des canvas des dessins soit responsive
+        print(event)
         self.canvas.config(width=event.width, height=event.height)
 
     def RunFenetre(self):
