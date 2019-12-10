8ab48fa27f77ddefd121c06ac7016fd3e1f935a1
Stormald <t.gds74@gmail.com>
2019-12-10 17:40:25 +0100

    ajout powerpoint kevin

--- Python/WorkSpaceThomas/createGraph.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from tkinter import ttk
-
-def CreateTab(tabControl):
-    TabName = ttk.Frame(tabControl)
-    tabControl.add(TabName, text="1")
-
-    ttk.Label(TabName, text="This is Tab 1").grid(column=0, row=0, padx=10, pady=10)
